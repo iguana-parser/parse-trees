063223e3eb73378559f7b3a601c913962732ff2c
Anastassija <A.Izmaylova@cwi.nl>
2016-03-15 01:49:11 +0100

    Revised the grammar builder.

--- src/main/scala/iguana/parsetrees/iggy/TermTraversal.scala
+++ src/main/scala/iguana/parsetrees/iggy/TermTraversal.scala
@@ -39,69 +39,25 @@ object TermTraversal {
   
   def build(term: Term, b: Actions): Object = {
     term match {
-      case NonterminalTerm(t, ns, _) => if (t.label == null || t.label.isEmpty()) {
-                                   if (ns.size == 1) return build(ns.head, b);
-                                   val x = skip(buildL(ns, b))
-                                   if (x.size() == 1) return x.get(0)
-                                   else
-                                     throw new RuntimeException("This rule should have a label: " + t)
-                                 } else if (t.label.toLowerCase.equals("start")) {
-                                   return build(ns.tail.head, b)
-                                 } else {
-                                   val x = buildL(ns, b)
-                                   refArrayOps(b.getClass.getMethod(t.head.toLowerCase(), asScalaBuffer(new java.util.ArrayList[Class[_]]):_*)
-                                               .invoke(null).getClass.getMethods) foreach { m => // Public, static method!
-                                     if (m.getName.equals(t.label.toLowerCase)) {
-                                       if (m.getParameterCount == x.size())
-                                         try {
-                                           return m.invoke(null, x: _*)
-                                         } catch {
-                                           case e: IllegalArgumentException =>
-                                             throw new RuntimeException("The matching method has not been found: "
-                                                         + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                           case e: InvocationTargetException => throw e.getTargetException
-                                         }
-                                       else {
-                                         val types = m.getParameterTypes;
-                                         if (types.length > x.size())
-                                           throw new RuntimeException("The matching method has not been found: "
-                                                       + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                         val y = new java.util.ArrayList[Object]
-                                         var i, j = 0
-                                         var len = x.size
-                                         asScalaBuffer(x) foreach { elem =>
-                                           if (j > types.length)
-                                             throw new RuntimeException("The matching method has not been found: "
-                                               + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                           if (elem.isInstanceOf[java.lang.String]
-                                                   && (j == types.length || types.apply(j) != elem.getClass)) {
-                                             len = len - 1;
-                                             if (len < types.length)
-                                               throw new RuntimeException("The matching method has not been found: "
-                                                           + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                           } else {
-                                             y.add(elem)
-                                             j = j + 1
-                                           }
-                                           i = i + 1
-                                         }
-                                         if (y.size != types.length)
-                                           throw new RuntimeException("The matching method has not been found: "
-                                                       + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                         try {
-                                           return m.invoke(null, y: _*)
-                                         } catch {
-                                           case e: IllegalArgumentException =>
-                                             throw new RuntimeException("The matching method has not been found: "
-                                                     + t.head + "." + t.label.toLowerCase + x.foldLeft("") { (a, el) => a + "," + el.getClass.getName })
-                                           case e: InvocationTargetException => throw e.getTargetException
-                                         }
-                                       }
-                                     }
-                                   }
-                                   throw new RuntimeException("The matching method has not been found: "
-                                                 + t.head + "." + t.label.toLowerCase + x.foldLeft(""){ (a, el) => a + "," + el.getClass.getName })
-                                 }
+      case NonterminalTerm(t, ns, _) if (t.label == null || t.label.isEmpty())
+        =>
+           if (ns.size == 1) return build(ns.head, b);
+           val x = skip(buildL(ns, b))
+           if (x.size() == 1) return x.get(0)
+           else
+             throw new RuntimeException("This rule should have a label: " + t)
+
+      case NonterminalTerm(t, ns, _) if (t.label != null && !t.label.isEmpty)
+        =>
+           if (t.label.toLowerCase.equals("start"))
+             return build(ns.tail.head, b)
+           else {
+             val x = buildL(ns, b)
+             val fm = b.getClass.getMethod(t.head.toLowerCase(), asScalaBuffer(new java.util.ArrayList[Class[_]]):_*)
+             refArrayOps(fm.invoke(null).getClass.getMethods) foreach { m =>
+               if (m.getName.equals(t.label.toLowerCase)) return invokeMethod(m, x, t.head, t.label) }
+             throw new RuntimeException(composeErrorMsg(t.head, t.label, x))
+           }
       case Star(ns)  => skip(buildL(ns, b, flatten = true))
       case Plus(ns)  => skip(buildL(ns, b, flatten = true))
       case Opt(n)    => val x = build(n, b)
@@ -123,31 +79,87 @@ object TermTraversal {
     }
     
   }
-  
-  def buildL(children: Seq[Term], b: Actions, flatten: Boolean = false): java.util.List[Object] = {
-    val l = new java.util.ArrayList[Object];
-    var i = 0
+
+  /**
+    * Skips layout and flattens immediate list children
+    */
+  def buildL(children: Seq[Term], b: Actions, flatten: Boolean = false) = {
+    val result = new java.util.ArrayList[Object];
     children foreach { child =>
-      if (i==0||i%2==0) { // Skip layout inserted between symbols
+      if (!isLayout(child)) {
         val x = build(child, b)
-        if (flatten && x.isInstanceOf[java.util.List[_]])
-          l.addAll(x.asInstanceOf[java.util.List[Object]])
+        if (x.isInstanceOf[java.util.List[_]] && flatten)
+          result.addAll(x.asInstanceOf[java.util.List[Object]])
         else
-          l.add(x)
+          result.add(x)
       }
-      i = i + 1
     }
-    l
+    result
   }
-  
+
+  /**
+    * Skips terminals unless there are only terminals in the list
+    */
   def skip(l: java.util.List[Object]): java.util.List[Object] = {
     if (l.isEmpty() || l.size() == 1) return l
     val x = new java.util.ArrayList[Object]
-    asScalaBuffer(l) foreach { elem =>
-      if (!elem.isInstanceOf[java.lang.String])
-        x.add(elem)
-    }
+    asScalaBuffer(l) foreach { elem => if (!elem.isInstanceOf[java.lang.String]) x.add(elem) }
+    if (x.isEmpty) return l;
     x
   }
+
+  def isLayout(term: Term): Boolean = {
+    if (term.isInstanceOf[NonterminalTerm])
+      term.asInstanceOf[NonterminalTerm].isLayout
+    else if (term.isInstanceOf[TerminalTerm])
+      term.asInstanceOf[TerminalTerm].isLayout
+    else
+      false;
+  }
+
+  /**
+    * Attempts to match the signature of a method (@param m) with arguments (@param args):
+    *     - if the number of arguments matches the number of parameters, tries to invoke the method
+    *     - if the number of arguments does not match (greater than) the number of parameters, tries to skip terminals
+    *       following the order of parameters and their declared types; if a terminal is expected, pass it,
+    *       otherwise skip it; one special case of trailing terminals in the argument list has to be taken care of
+    */
+  def invokeMethod(m: java.lang.reflect.Method, args: java.util.List[Object], head: String, label: String): Object = {
+    if (m.getParameterCount == args.size()) {
+      try
+        return m.invoke(null, args: _*)
+      catch {
+        case e: IllegalArgumentException => throw new RuntimeException(composeErrorMsg(head, label, args))
+        case e: IllegalAccessException => throw e
+        case e: InvocationTargetException => throw e.getTargetException
+      }
+    } else {
+      val types = m.getParameterTypes;
+      if (types.length > args.size()) throw new RuntimeException(composeErrorMsg(head, label, args))
+      val y = new java.util.ArrayList[Object]
+      var i = 0
+      var len = args.size
+      asScalaBuffer(args) foreach { elem =>
+        if (i > types.length) throw new RuntimeException(composeErrorMsg(head, label, args))
+        if (elem.isInstanceOf[java.lang.String] && (types.apply(i) != elem.getClass) || i == types.length) {
+          len -= 1;
+          if (len < types.length) throw new RuntimeException(composeErrorMsg(head, label, args))
+        } else {
+          y.add(elem)
+          i += 1
+        }
+      }
+      if (y.size != types.length) throw new RuntimeException(composeErrorMsg(head, label, args))
+      try
+        return m.invoke(null, y: _*)
+      catch {
+        case e: IllegalArgumentException => throw new RuntimeException(composeErrorMsg(head, label, args))
+        case e: InvocationTargetException => throw e.getTargetException
+      }
+    }
+  }
+
+  def composeErrorMsg(head: String, label: String, args: java.util.List[Object])
+    = "The matching method has not been found: $head . ${label.toLowerCase} (${args.foldLeft(\"\") {(a,el) => a + \",\" + el.getClass.getName}})"
   
 }
